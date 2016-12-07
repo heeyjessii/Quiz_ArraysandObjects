4c2ba835e7f47eda8b40c5157bc40711e3a9ef18
Jessica <11lopez.jessica@gmail.com>
2016-12-07 08:31:48 -1000

    hopefully I get some right

---
+++ quiz_arrayandobject.js
@@ -0,0 +1,46 @@
+/*
+
+Jessica Lopez
+December 7,2016
+Period 1
+
+*/
+
+//variable
+var myArray = ["Jessica", "Lopez", 10, "I'm Tired", true];
+
+
+console.log(myArray[2]);
+
+//function 
+var knight = "Arthur";
+
+function bridgeMonster(boy){
+  if ("boy" == "stranger danger") {
+    console.log("You Shall Not Pass!");
+  }
+}
+
+bridgeMonster(knight);
+
+//Object of Batman
+var batman = {
+  man : true,
+  utilityBelt : ["knife", "gun", "sword"],
+  vehicle : "batmobile",
+  sidekick : "robin"
+}
+
+function superhero(hero){
+  if (hero == "man"){
+    console.log(hero + "will use a " + utilityBelt[0] + " on the Joker with " + sidekick);
+  }
+  else{
+    console.log("No, I'm the Bat");
+  }
+}
+
+superhero(batman);
+
+
+
